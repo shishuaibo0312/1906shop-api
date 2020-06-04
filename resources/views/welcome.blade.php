9c67041be5dbe71e569cbf2b8917bd677cb3831e
yaoyao <yaoyao@localhost.localdomain>
2020-06-04 07:52:39 +0000

    debug

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    SHOPAPI
+                    API接口
                 </div>
 
                 <div class="links">
